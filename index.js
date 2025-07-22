391b533a3618f7d58b9269bbf1dd9d18f6d64082
Shashanka <poudelshashanka205@gmail.com>
2025-07-22 07:42:35 +0545

    latest changes

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     origin: [
         'http://localhost:5173',
-        ' https://blogapp-frontend-texas.vercel.app'
+        'https://blogapp-frontend-texas.vercel.app'
     ],
     method: ['GET', 'POST']
 }))
